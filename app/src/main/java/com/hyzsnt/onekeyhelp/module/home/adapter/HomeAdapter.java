0dd2062d1214be5fd7f613a8e21e39daf7098672
hxw_legend <13521632836@163.com>
2016-12-11 10:31:48 +0800

    修改适配器

--- app/src/main/java/com/hyzsnt/onekeyhelp/module/home/adapter/HomeAdapter.java
+++ app/src/main/java/com/hyzsnt/onekeyhelp/module/home/adapter/HomeAdapter.java
@@ -1,33 +1,54 @@
 package com.hyzsnt.onekeyhelp.module.home.adapter;
 
 import android.content.Context;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.hyzsnt.onekeyhelp.R;
+
+import butterknife.BindView;
 
 /**
  * Created by hyzs on 2016/12/10.
  */
 
 public class HomeAdapter extends RecyclerView.Adapter {
-    private Context context;
+    RecyclerView homeLrvHead;
+    private Context mContext;
+    private View v1;
 
-    public HomeAdapter(Context context) {
-        this.context = context;
+    public HomeAdapter(Context mContext) {
+        this.mContext = mContext;
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//        View view = LayoutInflater.from(context).inflate(R.layout.item_home_circum,parent,false);
-        View view = View.inflate(context, R.layout.item_home_circum, null);
-        return new ViewHolder(view);
+        if (viewType == 0) {
+            v1 = LayoutInflater.from(mContext).inflate(R.layout.item_home_lrv_head, parent, false);
+            return new HomeViewHolder(v1);
+        } else {
+            View v2 = LayoutInflater.from(mContext).inflate(R.layout.item_home_circum, parent, false);
+            return new HomeViewHolder(v2);
+        }
+
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        if(getItemViewType(position)==0){
+            homeLrvHead= (RecyclerView) v1.findViewById(R.id.home_lrv_head);
+            HomeHeadAdapter homeHeadAdapter=new HomeHeadAdapter(mContext);
+            LinearLayoutManager layoutManager = new LinearLayoutManager(mContext);
+            layoutManager.setOrientation(LinearLayout.HORIZONTAL);
+            homeLrvHead.setLayoutManager(layoutManager);
+            homeLrvHead.setAdapter(homeHeadAdapter);
+            homeLrvHead.setItemAnimator(new DefaultItemAnimator());
+        }
     }
 
     @Override
@@ -35,35 +56,20 @@ public class HomeAdapter extends RecyclerView.Adapter {
         return 20;
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder{
-
-        public ViewHolder(View itemView) {
+    static class HomeViewHolder extends RecyclerView.ViewHolder {
+        private RecyclerView rv;
+        public HomeViewHolder(View itemView) {
             super(itemView);
+
         }
     }
 
-    //    @Override
-//    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//
-//        View view = LayoutInflater.from(context).inflate(R.layout.item_home_circum,parent,false);
-//        return new HomeViewHolder(view);
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-//        if(holder instanceof HomeViewHolder){
-//            HomeViewHolder mHomeViewHolder= (HomeViewHolder)holder;
-//        }
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return 10;
-//    }
-//    public static class HomeViewHolder extends RecyclerViewAdapter.ViewHolder{
-//
-//        public HomeViewHolder(View itemView) {
-//            super(itemView);
-//        }
-//    }
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
 }
