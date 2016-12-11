27731c7ef14631aae732d3ffa79c3a0463ba5cb2
hxw_legend <13521632836@163.com>
2016-12-11 16:44:38 +0800

    home加入圈子

--- app/src/main/java/com/hyzsnt/onekeyhelp/module/home/fragment/HomeLoginFragment.java
+++ app/src/main/java/com/hyzsnt/onekeyhelp/module/home/fragment/HomeLoginFragment.java
@@ -3,14 +3,21 @@ package com.hyzsnt.onekeyhelp.module.home.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.hyzsnt.onekeyhelp.R;
 import com.hyzsnt.onekeyhelp.base.BaseFragment;
+import com.hyzsnt.onekeyhelp.module.home.adapter.HomeUnLoginAdapter;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,9 +25,13 @@ import java.util.List;
 public class HomeLoginFragment extends BaseFragment {
 
 
+    @BindView(R.id.home_login_rv)
+    RecyclerView homeLoginRv;
+
     public HomeLoginFragment() {
         // Required empty public constructor
     }
+
     @Override
     protected List<String> getParams() {
         return null;
@@ -28,7 +39,10 @@ public class HomeLoginFragment extends BaseFragment {
 
     @Override
     protected void initData(String content) {
-
+        HomeUnLoginAdapter mHomeLoginAdapter=new HomeUnLoginAdapter(getActivity());
+        homeLoginRv.setLayoutManager(new LinearLayoutManager(getActivity()));
+        homeLoginRv.setAdapter(mHomeLoginAdapter);
+        homeLoginRv.setItemAnimator(new DefaultItemAnimator());
     }
 
     @Override
@@ -44,5 +58,13 @@ public class HomeLoginFragment extends BaseFragment {
     @Override
     public String getA() {
         return null;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        ButterKnife.bind(this, rootView);
+        return rootView;
     }
 }
