8ae2b3e1aa6fa891218a029cf3a4bbc4b9f703c4
hxw_legend <13521632836@163.com>
2016-12-13 10:17:11 +0800

    详情页

---
+++ app/src/main/java/com/hyzsnt/onekeyhelp/module/home/activity/StateActivity.java
@@ -0,0 +1,19 @@
+package com.hyzsnt.onekeyhelp.module.home.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hyzsnt.onekeyhelp.R;
+import com.hyzsnt.onekeyhelp.base.BaseActivity;
+
+public class StateActivity extends BaseActivity{
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_state;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
