d2dc1624f49719038d1ccd881346aa74bc02dbb9
developer_zzw <developer_zzw@163.com>
2016-12-14 14:21:32 +0800

    增加全局Location对象

---
+++ app/src/main/java/com/hyzsnt/onekeyhelp/module/help/bean/LocationInfo.java
@@ -0,0 +1,64 @@
+package com.hyzsnt.onekeyhelp.module.help.bean;
+
+/**
+ * Created by 14369 on 2016/12/14.
+ */
+
+public class LocationInfo {
+	private int mLocType;
+	private String mTime;
+	private double mLatitude;
+	private double mLongitude;
+	private String mAddrStr;
+
+	public int getLocType() {
+		return mLocType;
+	}
+
+	public void setLocType(int locType) {
+		mLocType = locType;
+	}
+
+	public String getTime() {
+		return mTime;
+	}
+
+	public void setTime(String time) {
+		mTime = time;
+	}
+
+	public double getLatitude() {
+		return mLatitude;
+	}
+
+	public void setLatitude(double latitude) {
+		mLatitude = latitude;
+	}
+
+	public double getLongitude() {
+		return mLongitude;
+	}
+
+	public void setLongitude(double longitude) {
+		mLongitude = longitude;
+	}
+
+	public String getAddrStr() {
+		return mAddrStr;
+	}
+
+	public void setAddrStr(String addrStr) {
+		mAddrStr = addrStr;
+	}
+
+	@Override
+	public String toString() {
+		return "LocationInfo{" +
+				"mLocType=" + mLocType +
+				", mTime='" + mTime + '\'' +
+				", mLatitude=" + mLatitude +
+				", mLongitude=" + mLongitude +
+				", mAddrStr='" + mAddrStr + '\'' +
+				'}';
+	}
+}
