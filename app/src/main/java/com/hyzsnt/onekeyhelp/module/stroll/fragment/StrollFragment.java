a6e851d270d928a485c5bd6596360640dfc93cfd
一厘米 <2278380476@qq.com>
2016-12-14 10:50:05 +0800

    Merge branch 'master' of https://git.oschina.net/developer_zzw/OneKeyHelp_v3.0
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/dimens.xml

--- app/src/main/java/com/hyzsnt/onekeyhelp/module/stroll/fragment/StrollFragment.java
+++ app/src/main/java/com/hyzsnt/onekeyhelp/module/stroll/fragment/StrollFragment.java
@@ -9,11 +9,14 @@ import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.hyzsnt.onekeyhelp.R;
 import com.hyzsnt.onekeyhelp.base.BaseFragment;
 import com.hyzsnt.onekeyhelp.http.Api;
+import com.hyzsnt.onekeyhelp.http.HttpUtils;
+import com.hyzsnt.onekeyhelp.http.response.ResponseHandler;
 import com.hyzsnt.onekeyhelp.module.stroll.activity.CircleDetailsActivity;
 import com.hyzsnt.onekeyhelp.module.stroll.activity.CreateCircleActivity;
 import com.hyzsnt.onekeyhelp.module.stroll.activity.SeekCircleActivity;
@@ -30,6 +33,9 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import okhttp3.Call;
+
+import static com.hyzsnt.onekeyhelp.utils.JsonUtils.isSuccess;
 
 /**
  * Created by Administrator on 2016/12/9.
@@ -43,8 +49,8 @@ public class StrollFragment extends BaseFragment {
 	ImageView mImStrollSeek;
 	@BindView(R.id.re_stroll_header_list)
 	RecyclerView mReStrollHeaderList;
-	@BindView(R.id.tv_stroll_gragment_round)
-	TextView mTvStrollGragmentRound;
+	@BindView(R.id.tv_stroll_fragment_round)
+	TextView mTvStrollFragmentRound;
 	@BindView(R.id.tv_stroll_fragment_me)
 	TextView mTvStrollFragmentMe;
 	@BindView(R.id.ex_circle_fragment)
@@ -69,7 +75,7 @@ public class StrollFragment extends BaseFragment {
 		layoutManager.setOrientation(LinearLayout.HORIZONTAL);
 		mReStrollHeaderList.setLayoutManager(layoutManager);
 		//解析数据
-        if(JsonUtils.isSuccess(content)){
+        if(isSuccess(content)){
 	        Gson gson = new Gson();
 	        hotTagData = gson.fromJson(content, CircleHotTag.class);
 	        hotTagData.getList();
@@ -79,12 +85,35 @@ public class StrollFragment extends BaseFragment {
 	        String err = JsonUtils.getErrorMessage(content);
 	        LogUtils.e(err);
         }
+        //mTvStrollFragmentRound.setOnClickListener(this);
+       //获取周边信息
+		ArrayList<String> list1 = new ArrayList<>();
+		list1.add("3");
+		list1.add("39.923594");
+		list1.add("116.539995");
+		list1.add("");
+		list1.add("");
+		HttpUtils.post(Api.CIRCLE, Api.Circle.CIRCLELIST, list1, new ResponseHandler() {
+			@Override
+			public void onError(Call call, Exception e, int id) {
 
+			}
+
+			@Override
+			public void onSuccess(String response, int id) {
+				if(JsonUtils.isSuccess(response)){
+					Gson gson =new Gson();
+
+				}
+			}
+
+			@Override
+			public void inProgress(float progress, long total, int id) {
+
+			}
+		});
 
 		HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
-
-
-
 
 		CircleFragmentAdapter mCircleFragmentAdapter = new CircleFragmentAdapter(mActivity, map);
 		mExCircleFragment.setAdapter(new CircleFragmentAdapter(mActivity,map));
@@ -134,12 +163,17 @@ public class StrollFragment extends BaseFragment {
 				//跳转到创建圈子页面
 				mActivity.startActivity(new Intent(mActivity, SeekCircleActivity.class));
 				break;
-			case R.id.tv_stroll_gragment_round:
+			case R.id.tv_stroll_fragment_round:
+				mTvStrollFragmentRound.setBackgroundResource(R.drawable.skip_btn_on_bg);
+				mTvStrollFragmentMe.setBackgroundResource(R.drawable.skip_btn_on_bg);
+				mTvStrollFragmentMe.setText("88gh");
+				Toast.makeText(mActivity,"hhhh",Toast.LENGTH_LONG).show();
 				break;
 			case R.id.tv_stroll_fragment_me:
 				break;
 		}
 	}
+
 
 
 }
