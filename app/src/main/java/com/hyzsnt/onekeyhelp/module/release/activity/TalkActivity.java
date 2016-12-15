3a168da30f99b1cb975b216cffc91faacb25f29f
gao <changhexuefei@163.com>
2016-12-15 19:14:46 +0800

    发布界面的修改

--- app/src/main/java/com/hyzsnt/onekeyhelp/module/release/activity/TalkActivity.java
+++ app/src/main/java/com/hyzsnt/onekeyhelp/module/release/activity/TalkActivity.java
@@ -1,7 +1,9 @@
 package com.hyzsnt.onekeyhelp.module.release.activity;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -12,31 +14,70 @@ import android.widget.TextView;
 import com.hyzsnt.onekeyhelp.R;
 import com.hyzsnt.onekeyhelp.base.BaseActivity;
 import com.hyzsnt.onekeyhelp.module.home.activity.StateActivity;
-import com.hyzsnt.onekeyhelp.module.release.view.MyEditText;
+import com.hyzsnt.onekeyhelp.module.release.adapter.ChoosePhotoListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import cn.finalteam.galleryfinal.FunctionConfig;
+import cn.finalteam.galleryfinal.GalleryFinal;
+import cn.finalteam.galleryfinal.model.PhotoInfo;
+import cn.finalteam.galleryfinal.widget.HorizontalListView;
 
 
 public class TalkActivity extends BaseActivity {
-
+    private final int REQUEST_CODE_GALLERY = 1001;
+    private final int REQUEST_CODE_CAMERA = 1000;
 
     @BindView(R.id.btn_return)
     ImageButton mBtnReturn;
     @BindView(R.id.tv_msg)
     TextView mTvMsg;
     @BindView(R.id.et_title)
-    MyEditText mEtTitle;
+    EditText mEtTitle;
     @BindView(R.id.et_content)
     EditText mEtContent;
     @BindView(R.id.cbx)
     CheckBox mCbx;
     @BindView(R.id.btn_release)
     Button mBtnRelease;
-    //checkBox默认选中
-    boolean isChecked = true;
+    @BindView(R.id.horizontal)
+    HorizontalListView horizontalListView;
     @BindView(R.id.add_icon)
     TextView mAddIcon;
+    //checkBox默认选中
+    boolean isChecked = true;
+
+    private String mTitle;
+    private String mContent;
+    private List<PhotoInfo> photoList;
+    private ChoosePhotoListAdapter adapter;
+    private GalleryFinal.OnHanlderResultCallback mOnHanlderResultCallback = new GalleryFinal.OnHanlderResultCallback() {
+        @Override
+        public void onHanlderSuccess(int reqeustCode, List<PhotoInfo> resultList) {
+            if (resultList != null) {
+                switch (reqeustCode) {
+                    case REQUEST_CODE_GALLERY:
+                        photoList.clear();
+                        photoList.addAll(resultList);
+                        adapter.notifyDataSetChanged();
+                        break;
+                    case REQUEST_CODE_CAMERA:
+                        photoList.clear();
+                        photoList.addAll(resultList);
+                        adapter.notifyDataSetChanged();
+                        break;
+                }
+            }
+        }
+
+        @Override
+        public void onHanlderFailure(int requestCode, String errorMsg) {
+
+        }
+    };
 
 
     @Override
@@ -46,26 +87,28 @@ public class TalkActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+        //用户输入的标题信息
+        mTitle = mEtTitle.getText().toString();
+        //用户输入的内容信息
+        mContent = mEtContent.getText().toString();
+
+        photoList = new ArrayList<>();
+        adapter = new ChoosePhotoListAdapter(this, photoList);
+        horizontalListView.setAdapter(adapter);
+        horizontalListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(TalkActivity.this, "位置" + position, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
         Intent intent = getIntent();
         String tag = intent.getStringExtra("tag");
         if (tag.equals("iv_talk")) {
             mTvMsg.setText("发表说说");
-
         }
-        mCbx.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    //do something
-                } else {
-                    //do something else
-                }
-            }
-        });
-        mCbx.setChecked(isChecked);
-
     }
-
 
     @OnClick({R.id.btn_return, R.id.btn_release})
     public void onClick(View view) {
@@ -79,10 +122,16 @@ public class TalkActivity extends BaseActivity {
                 });
                 break;
             case R.id.btn_release:
+                //点击发布信息后，将文字信息和图片信息发送到详情页面
                 mBtnRelease.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        startActivity(new Intent(TalkActivity.this, StateActivity.class));
+                        Intent intent = new Intent(TalkActivity.this, StateActivity.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putString("mTitle", mTitle);
+                        bundle.putString("mContent", mContent);
+                        intent.putExtras(bundle);
+                        startActivity(intent);
                     }
                 });
                 break;
@@ -94,10 +143,35 @@ public class TalkActivity extends BaseActivity {
         mAddIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                FunctionConfig functionConfig = new FunctionConfig.Builder()
+                        .setMutiSelectMaxSize(3)
+                        .build();
 
+                int childCount = horizontalListView.getChildCount();
+                if ( childCount >= 1 && childCount < 3) {
+                    mAddIcon.setVisibility(View.VISIBLE);
+                }
+                if(childCount == 3){
+                    mAddIcon.setVisibility(View.GONE);
+                }
+                GalleryFinal.openGalleryMuti(REQUEST_CODE_GALLERY, functionConfig, mOnHanlderResultCallback);
             }
         });
+    }
 
-
+    @Override
+    protected void initListener() {
+        super.initListener();
+        mCbx.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    //do something
+                } else {
+                    //do something else
+                }
+            }
+        });
+        mCbx.setChecked(isChecked);
     }
 }
