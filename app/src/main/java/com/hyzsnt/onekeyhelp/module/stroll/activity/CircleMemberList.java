06c6de5320a830871cae5ee6d6a86a884fe5a383
一厘米 <2278380476@qq.com>
2016-12-15 18:29:05 +0800

    Merge branch 'master' of https://git.oschina.net/developer_zzw/OneKeyHelp_v3.0
    # Conflicts:
    #	app/src/main/java/com/hyzsnt/onekeyhelp/module/home/adapter/HomeUnLoginAdapter.java

---
+++ app/src/main/java/com/hyzsnt/onekeyhelp/module/stroll/activity/CircleMemberList.java
@@ -0,0 +1,17 @@
+package com.hyzsnt.onekeyhelp.module.stroll.activity;
+
+import com.hyzsnt.onekeyhelp.base.BaseActivity;
+
+public class CircleMemberList extends BaseActivity {
+
+
+	@Override
+	protected int getLayoutId() {
+		return 0;
+	}
+
+	@Override
+	protected void initData() {
+
+	}
+}
